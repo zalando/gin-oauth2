5f753d2440ef86e69d69c22c54f55660ee015d19
Sandor Szuecs <sandor.szuecs@zalando.de>
2016-03-02 17:19:13 +0100

    [FIX] test should use a file such that we can easily run tests

--- zalando/zalando_test.go
+++ zalando/zalando_test.go
@@ -3,6 +3,8 @@ package zalando
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
@@ -10,22 +12,51 @@ import (
 	"golang.org/x/oauth2"
 )
 
+// You have to have a current file $HOME/.chimp-token with only a
+// valid Zalando access token.
+var tokenFile string = fmt.Sprintf("%s/.chimp-token", os.Getenv("HOME"))
+
+func getToken() (string, error) {
+	file, err := os.Open(tokenFile)
+	if err != nil {
+		return "not a file", err
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "reading failed", err
+	}
+	return string(data), nil
+}
+
 func TestRequestTeamInfo(t *testing.T) {
 	ginoauth2.AuthInfoURL = OAuth2Endpoint.TokenURL
-	input := "YOUR_TOKEN_HERE"
+	accessToken, err := getToken()
+	if err != nil {
+		fmt.Printf("ERR: Could not get Access Token from file, caused by %q.", accessToken)
+		t.FailNow()
+	}
+
 	token := oauth2.Token{
-		AccessToken: input,
+		AccessToken: accessToken,
 		TokenType:   "Bearer",
 		Expiry:      time.Now().Add(time.Duration(600) * time.Second),
 	}
 	tc, err := ginoauth2.GetTokenContainer(&token)
 	if err != nil {
+		fmt.Printf("ERR: Could not get TokenContainer from ginoauth2.")
 		t.FailNow()
 	}
-	resp, _ := RequestTeamInfo(tc, TeamAPI)
+	resp, err := RequestTeamInfo(tc, TeamAPI)
+	if err != nil {
+		fmt.Printf("ERR: Could not get TeamInfo for TokenContainer from TeamAPI.")
+		t.FailNow()
+	}
 	var data []TeamInfo
 	err = json.Unmarshal(resp, &data)
 	if err != nil {
+		fmt.Printf("ERR: Could not unmarshal json data.")
 		t.FailNow()
 	}
 	fmt.Printf("%+v\n", data)
